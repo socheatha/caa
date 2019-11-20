89265696450dbe3c2dd59dfa2704d328e954ea9e
Buntheng <yin.buntheng@gmail.com>
2019-11-20 23:54:36 +0700

    Update about_us.blade.php

--- resources/views/frontend/about_us.blade.php
+++ resources/views/frontend/about_us.blade.php
@@ -100,7 +100,6 @@
                             <div role="tabpanel" class="tab-pane fade" id="mission_vision">
                                 <h3>Mission</h3>
                                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.</p>
-                                <br>
                                 <h3>Vision</h3>
                                 <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.</p>
                             </div><!-- end of block mission & vision -->
