6027afe7974bcf7f52f5706e1aecf40d84e10ea2
Buntheng <yin.buntheng@gmail.com>
2019-11-22 22:36:46 +0700

    Buntheng
    finished on about us page

--- resources/views/frontend/about_us.blade.php
+++ resources/views/frontend/about_us.blade.php
@@ -1,44 +1,42 @@
-@extends('layouts.app')
+@extends('layouts.app-2')
 
 @section('content')
 
-    <div class="container">
-        {{-- Block slider --}}
-        <section id="block-front-slider">
-            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                <ol class="carousel-indicators">
-                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-                </ol>
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img src="/images/sliders/6.jpg" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="/images/sliders/5.jpg" class="d-block w-100" alt="...">
-                    </div>
-                    <div class="carousel-item">
-                        <img src="/images/sliders/4.jpg" class="d-block w-100" alt="...">
-                    </div>
+    {{-- Block slider --}}
+    <section id="block-front-slider">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="/images/sliders/6.jpg" class="d-block w-100" alt="...">
                 </div>
-                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Previous</span>
-                </a>
-                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                    <span class="sr-only">Next</span>
-                </a>
+                <div class="carousel-item">
+                    <img src="/images/sliders/5.jpg" class="d-block w-100" alt="...">
+                </div>
+                <div class="carousel-item">
+                    <img src="/images/sliders/4.jpg" class="d-block w-100" alt="...">
+                </div>
             </div>
-        </section>
-
-
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </section>
+    <div class="container">
         <div class="row">
-            <div class="col-lg-9">
+            <div class="col-sm-8">
                 <section id="block-front-latest-news" class="block-content">
-                    <div class="title-block" style="border: 0px !important;">
-                        <ul class="nav nav-tabs" role="tablist">
+                    <div class="contents">
+                        <ul class="nav nav-tabs" id="navtab" role="tablist">
                             <li class="nav-item">
                                 <a class="nav-link show active" href="#introduction" role="tab" data-toggle="tab" aria-selected="true">Introduction</a>
                             </li>
@@ -69,18 +67,18 @@
                             <!-- block history -->
                             <div role="tabpanel" class="tab-pane fade" id="history">
                                 <div class="row">
-                                    <div class="col-lg-12">
+                                    <div class="col-sm-12">
                                         <h3>History</h3>
                                     </div>
-                                    <div class="col-lg-7">
+                                    <div class="col-sm-7">
                                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
                                     </div>
-                                    <div class="col-lg-5">
+                                    <div class="col-sm-5">
                                         <div class="small-article">
                                             <img src="/images/contents/about-us/8.jpg" class="thumbnail" alt="..."/>
                                         </div>
                                     </div>
-                                    <div class="col-lg-12">
+                                    <div class="col-sm-12">
                                         <h3>Occupation</h3>
                                         <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages.</p>
                                         <div class="small-article">
@@ -117,19 +115,11 @@
                             </div><!-- end of block organization chart -->
                         </div>
                     </div>
-                    <div class="content">
-                        <div class="row">
-                            
-                        </div>
-                    </div>
-
                 </section>
             </div>
-            <div class="col-lg-3">
-
+            <div class="col-sm-4">
+                @include('include.side_bar_right');
             </div>
         </div>
-
     </div>
-    <br><br><br>
 @endsection
