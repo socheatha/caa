2577786581e4c5bedb22792c40b766a94cffabde
socheatha <teysocheatha@gmail.com>
2019-12-02 22:39:17 +0700

    add migration donation table

---
+++ database/migrations/2019_12_02_153625_create_donations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDonationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('donations', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->string('seo_keywords');
+            $table->string('seo_description');
+
+            $table->text('detail_en');
+            $table->text('detail_kh');
+            $table->text('detail_my');
+            $table->text('detail_sa');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('donations');
+    }
+}
